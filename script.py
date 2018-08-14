73dc341d2122a544de11a8c561ee2f72800e77d1
danrevah <danrevah89@gmail.com>
2018-08-14 21:19:07 +0300

    Extract fn

--- script.py
+++ script.py
@@ -1,24 +1,13 @@
 import sys
 import turicreate as tc
 
-print("[1] Boosted decision tree\n"
-      "[2] Decision tree\n"
-      "[3] Nearest neighbour\n")
-
-try:
-    num = int(input("Select ML Algorithm: "))
-
-    if not num or num < 1 or num > 3:
-        print("\nError: Please select a number between 1 to 3.")
-        sys.exit(1)
-
-
+def execute_algorithm(algo):
     data =  tc.SFrame('dataset.csv')
     train_data, test_data = data.random_split(0.8)
 
-    if num == 1:
+    if algo == 1:
         model = tc.boosted_trees_classifier.create(train_data, target='Result', max_iterations=2, max_depth = 3)
-    elif num == 2:
+    elif algo == 2:
         model = tc.decision_tree_classifier.create(train_data, target='Result')
     else:
         model = tc.nearest_neighbor_classifier.create(train_data, target='Result')
@@ -26,8 +15,20 @@ try:
     predictions = model.classify(test_data)
     results = model.evaluate(test_data)
 
+    return results['accuracy']
 
-    print("\n\n\n>>> Accuracy         : %s" % results['accuracy'])
+try:
+    print("[1] Boosted decision tree\n"
+          "[2] Decision tree\n"
+          "[3] Nearest neighbour\n")
+
+    num = int(input("Select ML Algorithm: "))
+
+    if not num or num < 1 or num > 3:
+        raise ValueError()
+
+    accuracy = execute_algorithm(num)
+    print("\n\n\n>>> Accuracy         : %s" % accuracy)
 
 except ValueError:
     print("\nError: Please select a number between 1 to 3.")
